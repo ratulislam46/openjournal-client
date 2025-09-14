0cb061e086889013357cd1f74e7bb5c6cadd0161
ratulislam46 <arfanratul46@gmail.com>
2025-09-15 02:04:40 +0600

    updated subscribe section and also responsive

--- src/Home/NewLetter.jsx
+++ src/Home/NewLetter.jsx
@@ -14,7 +14,7 @@ const NewLetter = () => {
     return (
         <div className='py-20 pt-10'>
             <div data-aos="zoom-in-up"
-                className='w-11/12 mx-auto py-16 rounded-md bg-primary/5 px-36'>
+                className='w-11/12 mx-auto py-16 rounded-md bg-primary/5 px-6 lg:px-36'>
                 <h1 className='text-xl md:text-3xl lg:text-5xl font-serif uppercase text-center'>
                     Subscribe Newsletter
                     {/* <span className='text-2xl lg:text-4xl'>
@@ -31,9 +31,9 @@ const NewLetter = () => {
                 </h1>
                 <p className='text-center mt-2 text-base-content/60 text-xs md:text-md lg:text-xl'>Join our readers’ community and never miss a new post, story, or update from Open Journal.</p>
                 <form onSubmit={handleNewsLetter}
-                    className='mt-6 text-center'>
-                    <input type="email" className='input w-[50%] py-8 bg-base-100' placeholder='Enter Your Email' required />
-                    <input type="submit" value="Submit" className='btn btn-primary pb-10 pt-6 w-[20%]' />
+                    className='mt-6 text-center flex items-center justify-center'>
+                    <input type="email" className='input py-8 bg-base-100' placeholder='Enter Your Email' required />
+                    <input type="submit" value="Submit" className='pb-6 pt-5 px-4 bg-primary border rounded-br-md rounded-tr-md rounded-tl-md rounded-bl-md border-none cursor-pointer' />
                 </form>
             </div>
         </div>
