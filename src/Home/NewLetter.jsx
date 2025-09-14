bc69c5b80394b1e2cd78e27c2f01a9e3e0915e97
ratulislam46 <arfanratul46@gmail.com>
2025-09-14 21:05:07 +0600

    updated newsletter section

--- src/Home/NewLetter.jsx
+++ src/Home/NewLetter.jsx
@@ -13,9 +13,11 @@ const NewLetter = () => {
 
     return (
         <div className='py-20 pt-10'>
-            <div data-aos="zoom-in-up" className='w-11/12 mx-auto'>
-                <h1 className='text-5xl lg:text-6xl font-serif uppercase text-center'>N
-                    <span className='text-2xl lg:text-4xl'>
+            <div data-aos="zoom-in-up"
+                className='w-11/12 mx-auto py-16 rounded-md bg-primary/5 px-36'>
+                <h1 className='text-xl md:text-3xl lg:text-5xl font-serif uppercase text-center'>
+                    Subscribe Newsletter
+                    {/* <span className='text-2xl lg:text-4xl'>
                         <Typewriter
                             words={["ews letter"]}
                             loop={false}
@@ -25,11 +27,13 @@ const NewLetter = () => {
                             deleteSpeed={100}
                             delaySpeed={1000}
                         />
-                    </span>
+                    </span> */}
                 </h1>
-                <form onSubmit={handleNewsLetter} className='mt-16'>
-                    <input type="email" className='input w-[67%] py-8' placeholder='Enter Your Email' required />
-                    <input type="submit" value="Submit" className='btn btn-outline btn-error pb-10 pt-6 w-[30%]' />
+                <p className='text-center mt-2 text-base-content/60 text-xs md:text-md lg:text-xl'>Join our readers’ community and never miss a new post, story, or update from Open Journal.</p>
+                <form onSubmit={handleNewsLetter}
+                    className='mt-6 text-center'>
+                    <input type="email" className='input w-[50%] py-8 bg-base-100' placeholder='Enter Your Email' required />
+                    <input type="submit" value="Submit" className='btn btn-primary pb-10 pt-6 w-[20%]' />
                 </form>
             </div>
         </div>
